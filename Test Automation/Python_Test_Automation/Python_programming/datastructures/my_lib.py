ffdbc4aa964a363252c5bf5321709d910cd49962
Gayathri2305200 <kurapatigayathri3@gmail.com>
2024-03-17 23:06:52 +0530

    task

--- Test Automation/Python_Test_Automation/Python_programming/datastructures/my_lib.py
+++
@@ -1,5 +0,0 @@
-def frequency_of_words(str):
-    l=list(str.split())
-    s=set(l)
-    d={i:l.count(i) for i in s}
-    return d
--- Test Automation/Python_Test_Automation/Python_programming/searching_sorting/__init__.py
+++
@@ -1,3 +0,0 @@
-a ={} 
-d=a.fromkeys(['a', 'b', 'c', 'd'], 98)
-print (d)
